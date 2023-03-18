97a6e34cac9cc27a4de935abd07c8e901cd7abdf
T14D3 <sundmacher.tiade@gmail.com>
2023-03-18 13:27:24 +0100

    Initial commit

---
+++ src/main/java/io/github/t14d3core/T14D3Core.java
@@ -0,0 +1,17 @@
+package io.github.t14d3core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class T14D3Core extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
